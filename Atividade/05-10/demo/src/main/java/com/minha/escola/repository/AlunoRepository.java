b72e70dd9d5f3bf6583fae24895451e54a4c0978
Weslley <weslleynoog@hotmail>
2020-10-05 14:29:06 -0300

    Atividades do dia 5/10

---
+++ Atividade/05-10/demo/src/main/java/com/minha/escola/repository/AlunoRepository.java
@@ -0,0 +1,14 @@
+package com.minha.escola.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+import com.minha.escola.demo.Aluno;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long>{
+	
+	
+}
